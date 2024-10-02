0dbb4e603c2c2469d5735dc1223473989cbfd50e
Gungun Gupta <gupta.gungun10@gmail.com>
2024-10-02 14:58:09 +0530

    modify app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    # return render_template('home.html')
+    return 'helloworld'
 
 
 @app.route('/upload-image', methods=['POST'])
